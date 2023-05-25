b0b66b6e8ed29600ef603b37c029d610d484cec7
marvin <marvin.lanz@besonet.ch>
2023-05-25 13:06:25 +0200

    Create VK Instance

--- main.cpp
+++ main.cpp
@@ -1,35 +1,138 @@
 ﻿#define GLFW_INCLUDE_VULKAN
+
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
 
-int main() {
-    glfwInit();
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+class HelloTriangleApplication {
+private:
+    GLFWwindow *m_window;
+    VkInstance m_vkInstance;
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        m_window = glfwCreateWindow(WIDTH, HEIGHT, "Moonlight", nullptr, nullptr);
+    }
 
-    glfwTerminate();
+    void initVulkan() {
+        createInstance();
+    }
 
-    return 0;
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Moonlight";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "Moonlight";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto &extension: extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        if(!checkGLFWCompatability(glfwExtensions, glfwExtensionCount, extensions)){
+            throw std::runtime_error("missing required extension!");
+        }
+
+        if (vkCreateInstance(&createInfo, nullptr, &m_vkInstance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    struct compare {
+        const char* extension;
+
+        compare(const char* extension) : extension(extension) {}
+
+        bool operator()(VkExtensionProperties &toCompare) {
+            return (strcmp(toCompare.extensionName, extension) == 0);
+        }
+    };
+
+    bool checkGLFWCompatability(const char **glfwExtensions, uint32_t extensionCount,
+                                std::vector<VkExtensionProperties> availableExtensions) {
+
+        if (glfwExtensions == nullptr) {
+            std::cout << "required extensions not available";
+            return false;
+        }
+
+        for (int i = 0; i < extensionCount; ++i) {
+            const char *currentExtension = glfwExtensions[i];
+            if (std::find_if(availableExtensions.begin(), availableExtensions.end(), compare(currentExtension)) ==
+                availableExtensions.end()) {
+                std::cout << "required extensions not supported: " << currentExtension << '\n';
+                return false;
+            }
+            std::cout << "Found " << currentExtension;
+        }
+        
+        return true;
+    }
+    
+    void mainLoop() {
+        while (!glfwWindowShouldClose(m_window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(m_vkInstance, nullptr);
+        
+        glfwDestroyWindow(m_window);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
