cf53a67cd4e7bb50156dae032e4783c7e0a9ea19
marvin <marvin.lanz@besonet.ch>
2023-05-26 15:49:43 +0200

    Added Image views

--- main.cpp
+++ main.cpp
@@ -80,6 +80,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createGraphicsPipeline();
     }
 
     void createSurface() {
@@ -384,6 +385,37 @@ private:
     }
     
     void createImageViews() {
+        m_swapChainImageViews.resize(m_swapChainImages.size());
+
+        int i = 0;
+        for (auto & m_swapChainImage : m_swapChainImages) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = m_swapChainImage;
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = m_swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+            
+            
+            if (vkCreateImageView(m_vkDevice, &createInfo, nullptr, &m_swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+            i++;
+        }
+        
+    }
+
+    void createGraphicsPipeline() {
 
     }
 
@@ -584,6 +616,10 @@ private:
 
     void cleanup() {
 
+        for (auto imageView : m_swapChainImageViews) {
+            vkDestroyImageView(m_vkDevice, imageView, nullptr);
+        }
+        
         vkDestroySwapchainKHR(m_vkDevice, m_vkSwapChain, nullptr);
         vkDestroyDevice(m_vkDevice, nullptr);
 
